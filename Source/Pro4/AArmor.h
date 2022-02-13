313d7e015100ff112d7ffb3201e67701235a461a
DongCheon Kwon <movesky98@naver.com>
2022-02-13 22:41:14 +0900

    2022-02-13(병합 오류 수정)

---
+++ Source/Pro4/AArmor.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ABaseItem.h"
+#include "AArmor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRO4_API AAArmor : public AABaseItem
+{
+	GENERATED_BODY()
+	
+public:
+
+	AAArmor();
+
+	enum class ArmorType : uint8
+	{
+		Helmet,		// «Ô∏‰
+		Flak_Jacket	// πÊ≈∫¡∂≥¢
+	};
+
+	ArmorType CurrentArmor;
+
+protected:
+	void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+	void RandomSpawn();
+};
--- Source/Pro4/Pro4GameMode.cpp
+++ Source/Pro4/Pro4GameMode.cpp
@@ -3,10 +3,43 @@
 #include "Pro4GameMode.h"
 #include "Pro4PlayerController.h"
 #include "Pro4Character.h"
+#include <Blueprint/UserWidget.h>
 
 APro4GameMode::APro4GameMode()
 {
 	// set default pawn class to our Blueprinted character
 	DefaultPawnClass = APro4Character::StaticClass();
 	PlayerControllerClass = APro4PlayerController::StaticClass();
+}
+
+void APro4GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 게임이 시작될 때 UI를 표시하도록 만들어줌.
+	ChangeMenuWidget(StartingWidgetClass);
+}
+
+void APro4GameMode::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	// 먼저 CurrentWidget이 비어있는지 확인
+	if (CurrentWidget != nullptr)
+	{
+		// 비어있지 않다면 화면에서 UI를 제거한 후 CurrentWidget를 비워줌
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	// 이후 매개 변수로 받은 NewWidgetClass가 유효한지 검사
+	if (NewWidgetClass != nullptr)
+	{
+		// CreateWidget 함수로 새 위젯을 만들어 CurrentWidget에 저장
+		CurrentWidget = CreateWidget(GetWorld(), NewWidgetClass);
+
+		// 이후 뷰포트에 표시하도록 만들어준다.
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
 }
