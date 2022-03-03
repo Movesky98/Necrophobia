39c38a3ab3d181543fa7e909a58345636ff90de1
DongCheon Kwon <movesky98@naver.com>
2022-03-03 12:06:56 +0900

    2022-03-03(클라이언트에 UI 적용)
    기존에 Gamemode에 있던 UI 생성 로직을 CharacterController로 옮겨서 각 클라이언트마다 UI가 보이도록 설정하였습니다.

--- Source/Pro4/Pro4PlayerController.cpp
+++ Source/Pro4/Pro4PlayerController.cpp
@@ -2,12 +2,51 @@
 
 
 #include "Pro4PlayerController.h"
+#include "PlayerDefaultWidget.h"
+
+APro4PlayerController::APro4PlayerController()
+{
+	static ConstructorHelpers::FClassFinder<UPlayerDefaultWidget> BP_PlayerUI(TEXT("/Game/UI/PlayerUI.PlayerUI_C"));
+	if (BP_PlayerUI.Succeeded())
+	{
+		StartingWidgetClass = BP_PlayerUI.Class;
+	}
+}
 
 void APro4PlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+	if (IsLocalPlayerController())
+	{
+		// 게임이 시작될 때 UI를 표시하도록 만들어줌.
+		ChangeMenuWidget(StartingWidgetClass);
 
-	// SetInputMode에 FInputModeGameAndUI를 넣어주면
-	// 게임과 UI 모두 입력 가능한 상태가 된다.
-	SetInputMode(FInputModeGameOnly());
+		// SetInputMode에 FInputModeGameAndUI를 넣어주면
+		// 게임과 UI 모두 입력 가능한 상태가 된다.
+		SetInputMode(FInputModeGameOnly());
+	}
+}
+
+void APro4PlayerController::ChangeMenuWidget(TSubclassOf<class UPlayerDefaultWidget> NewWidgetClass)
+{
+	// 먼저 CurrentWidget이 비어있는지 확인
+	if (CurrentWidget != nullptr)
+	{
+		// 비어있지 않다면 화면에서 UI를 제거한 후 CurrentWidget를 비워줌
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	// 이후 매개 변수로 받은 NewWidgetClass가 유효한지 검사
+	if (NewWidgetClass != nullptr)
+	{
+		// CreateWidget 함수로 새 위젯을 만들어 CurrentWidget에 저장
+		CurrentWidget = CreateWidget<UPlayerDefaultWidget>(GetWorld(), NewWidgetClass);
+
+		// 이후 뷰포트에 표시하도록 만들어준다.
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
 }
--- Source/Pro4/Pro4PlayerController.h
+++ Source/Pro4/Pro4PlayerController.h
@@ -14,7 +14,20 @@ class PRO4_API APro4PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	APro4PlayerController();
+	
+	UFUNCTION(BlueprintCallable, Category = UMG_Game)
+	void ChangeMenuWidget(TSubclassOf<class UPlayerDefaultWidget> NewWidgetClass);
+	
 protected:
 	void BeginPlay() override;
 
+	// 게임이 시작될 때 표시될 UI 위젯을 저장할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<class UPlayerDefaultWidget> StartingWidgetClass;
+
+	// 현재 화면에 표시되고 있는 UI 위젯을 저장하고 있을 포인터 변수
+	UPROPERTY()
+	class UPlayerDefaultWidget* CurrentWidget;
 };
