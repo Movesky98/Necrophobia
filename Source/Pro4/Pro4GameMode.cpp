d089fe70758fe80d269657bf916119c041e961d9
Move <movesky98@naver.com>
2022-07-21 10:04:19 +0900

    2022-07-21(GameMode Update, Delay using Steamworks API)

--- Source/Pro4/Pro4GameMode.cpp
+++ Source/Pro4/Pro4GameMode.cpp
@@ -3,10 +3,11 @@
 #include "Pro4GameMode.h"
 #include "Pro4PlayerController.h"
 #include "Pro4Character.h"
+/*
 #include <Steamworks/Steamv153/sdk/public/steam/steam_api.h>
-// 스팀 라이브러리를 명시적으로 연결함
+ 스팀 라이브러리를 명시적으로 연결함
 #pragma comment(lib, "ThirdParty/Steamworks/Steamv153/sdk/redistributable_bin/win64/steam_api64.lib")
-
+*/
 
 
 APro4GameMode::APro4GameMode()
@@ -19,11 +20,12 @@ void APro4GameMode::BeginPlay()
 
 {
 	Super::BeginPlay();
-	//if (!SteamAPI_RestartAppIfNecessary(480)) // 앱 ID로 대체 
-	//{
-		// false가 나올 경우, Steam 클라이언트에서 게임을 실행한 것으로 인식함 
-	//	UE_LOG(Pro4, Error, TEXT("SteamAPI_RestartAppIfNecessary Successed"));
-	//}
+	/*
+	if (!SteamAPI_RestartAppIfNecessary(480)) // 앱 ID로 대체 
+	{
+		false가 나올 경우, Steam 클라이언트에서 게임을 실행한 것으로 인식함 
+		UE_LOG(Pro4, Error, TEXT("SteamAPI_RestartAppIfNecessary Successed"));
+	}
 
 	if(!SteamAPI_Init())
 	{
@@ -33,4 +35,5 @@ void APro4GameMode::BeginPlay()
 	{
 		UE_LOG(Pro4, Error, TEXT("SteamAPI_Init Successed"));
 	}
+	*/
 }
