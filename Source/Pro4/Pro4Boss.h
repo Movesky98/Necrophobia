34ff97119976d5dbb93d7aed9499d99bb4c28a2e
Move <movesky98@naver.com>
2022-11-17 00:16:22 +0900

    2022-11-17 (보스 피격 구현)

--- Source/Pro4/Pro4Boss.h
+++ Source/Pro4/Pro4Boss.h
@@ -28,6 +28,10 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/* Zombie Attack */
+	UFUNCTION()
+	void DrawAttackField();
+
 	void MovementSetting();
 	virtual void PostInitializeComponents() override;
 	void Attack();
@@ -37,11 +41,21 @@ public:
 	{
 		return IsAppear;
 	}
+
 	// 블랙보드에 공격이 끝낫음을 콜백해주기 위한 변수
 	FOnAttackEndDelegate OnAttackEnd;
 
 private:
+	UPROPERTY(Replicated, VisibleInstanceOnly, BlueprintReadOnly, Category = State, Meta = (AllowPrivateAccess = true))
+	float CurrentHP;
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = State, Meta = (AllowPrivateAccess = true))
+	float Damage;
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = State, Meta = (AllowPrivateAccess = true))
+	float Velocity;
+
 	// 애니메이션 컨트롤을 위한 변수
+	UPROPERTY(Replicated)
+	bool IsDead;
 	bool IsAttacking;
 	bool IsMontagePlay;
 	bool IsAppear;
@@ -49,12 +63,27 @@ private:
 
 	// 애니메이션 적용을 위한 클래스 변수
 	UPROPERTY()
-		class UBossAnimInstance* BossAnim;
+	class UBossAnimInstance* BossAnim;
+
+	/* Zombie Get Damaged */
+	void ZombieGetDamaged(float _Damage, AActor* AttackActor);
+
+	UFUNCTION(Server, Reliable)
+	void ZombieGetDamagedOnServer(float _Damage, AActor* AttackActor);
+
+	void Dead();
+
+	/* 몽타주를 전체 클라이언트에서 실행하는 함수 */
+	UFUNCTION(Server, Reliable)
+	void PlayMontageOnServer(UAnimMontage* AnimationMontage, uint16 SectionNumber = 0);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void PlayMontageOnClient(UAnimMontage* AnimationMontage, uint16 SectionNumber = 0);
 
 	// 몽타주 종료시 콜되는 함수
 	UFUNCTION()
-		void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+	void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted);
 
 	UFUNCTION()
-		void OnAppearMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+	void OnAppearMontageEnded(UAnimMontage* Montage, bool bInterrupted);
 };
--- Source/Pro4/Pro4Zombie.cpp
+++ Source/Pro4/Pro4Zombie.cpp
@@ -29,7 +29,7 @@ APro4Zombie::APro4Zombie()
 	ZombieCollision->SetCapsuleHalfHeight(150.0f);
 	ZombieCollision->SetCapsuleRadius(150.0f);
 
-	GetCapsuleComponent()->SetCollisionProfileName(TEXT("Zombie"));
+	GetMesh()->SetCollisionProfileName(TEXT("Zombie"));
 
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh>SK_Zombie(TEXT("/Game/Character_Animation/Zombie/NormalMaleZombie/attack.attack"));
 	if (SK_Zombie.Succeeded())
